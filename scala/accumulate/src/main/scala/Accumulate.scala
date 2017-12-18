fd6e30544c0ae94b9f1d0cb5c074fc10899a66b1
Michael Ripley <Michael.Ripley@walmart.com>
2017-12-17 23:46:07 -0600

    don't use .map in accumulate

--- scala/accumulate/src/main/scala/Accumulate.scala
+++ scala/accumulate/src/main/scala/Accumulate.scala
@@ -1,5 +1,7 @@
 class Accumulate {
-  def accumulate[A, B](f: (A) => B, list : List[A]): List[B] = {
-    list.map(f)
-  }
+
+  // apparently list.map(f) is too easy and I must reinvent the wheel here
+  // apparently "idiomatic" language features are ok, it's just library functions that are not
+  def accumulate[A, B](f: (A) => B, list : List[A]): List[B] = for (e <- list) yield f(e)
+  // wow such language feature
 }
