bf0a335f66841641b71ed51301a140b5d6d4db9b
Michael Ripley <Michael.Ripley@walmart.com>
2017-12-18 18:27:26 -0600

    start gigasecond

---
+++ scala/gigasecond/src/main/scala/Gigasecond.scala
@@ -0,0 +1,11 @@
+import java.time.LocalDate
+import java.time.LocalDateTime
+
+object Gigasecond {
+
+  def gigasecond = 1000000000
+
+  def add(startDate: LocalDate): LocalDateTime = ???
+
+  def add(startDateTime: LocalDateTime): LocalDateTime = ???
+}
