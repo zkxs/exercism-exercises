9db0d920099c1624ad53835c6743a8403896d349
Michael Ripley <zkxs00@gmail.com>
2018-01-13 10:28:26 -0600

    finish prime-factors

--- scala/prime-factors/src/main/scala/PrimeFactors.scala
+++ scala/prime-factors/src/main/scala/PrimeFactors.scala
@@ -1,9 +1,37 @@
+import scala.annotation.tailrec
+
 object PrimeFactors {
-  def factors(n: Int): List[Int] = {
-    ???
+
+  // bootstrap our recursive function
+  def factors(n: Long): List[Int] = {
+    if (n < 2) {
+      List.empty[Int]
+    } else {
+      factors(n, List.empty[Int])
+    }
   }
 
-  def isPrime(n: Long): Boolean = {
-    
+  // recursively find all prime factors
+  @tailrec
+  def factors(n: Long, factorList: List[Int]): List[Int] = {
+    val factor = getFactor(n)
+    val newFactorList = factorList :+ factor
+    val remaining = n / factor
+
+    if (remaining == 1) { // check for end-condition
+      newFactorList
+    } else {
+      factors(remaining, newFactorList)
+    }
+  }
+
+  /**
+    * Get the lowest prime factor of a number
+    * @param n A number
+    * @return the number's lowest prime factor
+    */
+  def getFactor(n: Long): Int = {
+    // We could do a time-memory trade-off here by keeping a cache of prime numbers
+    Stream.from(2).find(n % _ == 0).get
   }
 }
