5323ffb28f9f8ce8680edb2ffdb27c6f69a6137d
Michael Ripley <Michael.Ripley@walmart.com>
2018-01-29 16:08:21 -0600

    finish all-your-base

--- scala/all-your-base/src/main/scala/AllYourBase.scala
+++ scala/all-your-base/src/main/scala/AllYourBase.scala
@@ -1,9 +1,45 @@
+import scala.collection.mutable.MutableList
+
 object AllYourBase {
+
     def rebase(inputBase: Int, number: List[Int], outputBase: Int): Option[List[Int]] = {
-        try {
-            number.zipWithIndex.map(t => (t._1, number.size - t._2 - 1))
-        } catch {
-            case e: IllegalArgumentException => None
+        numberToInt(inputBase, number).flatMap(n => intToNumber(n, outputBase))
+    }
+
+    def numberToInt(inputBase: Int, number: List[Int]): Option[Int] = {
+        if (inputBase >= 2
+            && number.nonEmpty
+            && number(0) != 0
+            && !number.exists(_ < 0)
+            && !number.exists(_ >= inputBase)
+        ) {
+            Some(
+                number
+                    .zipWithIndex
+                    .map(t => (t._1, number.size - t._2 - 1))
+                    .map(t => {
+                        val (n, place) = t
+                        n * Math.pow(inputBase, place).toInt
+                    })
+                    .sum
+            )
+        } else {
+            None
         }
     }
+
+    def intToNumber(number: Int, outputBase: Int): Option[List[Int]] = {
+        if (outputBase >= 2) {
+            val list = MutableList.empty[Int]
+            var r = number
+            while (r != 0) {
+                (r % outputBase) +=: list // prepend to list
+                r /= outputBase
+            }
+            Some(list.toList)
+        } else {
+            None
+        }
+    }
+
 }
